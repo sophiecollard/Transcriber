ebe167500262aae289290b4aac0fa9e94b2a18dc
Sophie Collard <sophie.collard@hotmail.fr>
2019-09-13 20:26:06 +0100

    Implement revised Hangeul romanization (part 2)
    This commit implements some of the special provisions described in
    https://en.wikipedia.org/wiki/Revised_Romanization_of_Korean#Special_provisions
    
    Specifically, it implements cases for final consonants 'ㄱ' (giyeok) to
    'ㅇ' (ieung) and initial consonants 'ㅇ' (ieung) to 'ㅁ' (mieum).

--- src/main/scala/com/github/sophiecollard/transliterator/transliteration/HangeulRomanizer.scala
+++ src/main/scala/com/github/sophiecollard/transliterator/transliteration/HangeulRomanizer.scala
@@ -38,25 +38,27 @@ object HangeulRomanizer extends Transliterator[HangeulText, RomanizedText] {
           transliterateInitialInContext(maybePrecedingFinal, initial),
           transliterateMedial(medial)
         )
+      case IMM(initial, medial, secondMedial) =>
+        Monoid.combineAll(
+          transliterateInitialInContext(maybePrecedingFinal, initial),
+          transliterateMedial(medial),
+          transliterateMedial(secondMedial)
+        )
       case IMF(initial, medial, final_) =>
         Monoid.combineAll(
           transliterateInitialInContext(maybePrecedingFinal, initial),
           transliterateMedial(medial),
           transliterateFinalInContext(final_, maybeFollowingInitial)
         )
+      case IMMF(initial, medial, secondMedial, final_) =>
+        Monoid.combineAll(
+          transliterateInitialInContext(maybePrecedingFinal, initial),
+          transliterateMedial(medial),
+          transliterateMedial(secondMedial),
+          transliterateFinalInContext(final_, maybeFollowingInitial)
+        )
       case _ =>
         throw new RuntimeException("not implemented")
-    }
-
-  private def transliterateInitialInContext(
-    maybePrecedingFinal: Option[HangeulLetter.Consonant],
-    initial: HangeulLetter.Consonant
-  ): Vector[RomanLetter] =
-    (maybePrecedingFinal, initial) match {
-      case (Some(HangeulLetter.ㄹ), HangeulLetter.ㄹ) =>
-        Vector(L)
-      case _ =>
-        transliterateInitial(initial)
     }
 
   private def transliterateFinalInContext(
@@ -64,10 +66,45 @@ object HangeulRomanizer extends Transliterator[HangeulText, RomanizedText] {
     maybeFollowingInitial: Option[HangeulLetter.Consonant]
   ): Vector[RomanLetter] =
     (`final`, maybeFollowingInitial) match {
+      case (HangeulLetter.ㄱ, Some(HangeulLetter.ㅇ)) =>
+        Vector(G)
+      case (HangeulLetter.ㄱ, Some(HangeulLetter.ㄴ) | Some(HangeulLetter.ㄹ) | Some(HangeulLetter.ㅁ)) =>
+        Vector(N, G)
+      case (HangeulLetter.ㄷ, Some(HangeulLetter.ㅇ)) =>
+        Vector(D) // This can also sometimes be J
+      case (HangeulLetter.ㄷ, Some(HangeulLetter.ㄴ) | Some(HangeulLetter.ㄹ) | Some(HangeulLetter.ㅁ)) =>
+        Vector(N)
       case (HangeulLetter.ㄹ, Some(HangeulLetter.ㅇ)) =>
         Vector(R)
+      case (HangeulLetter.ㅂ, Some(HangeulLetter.ㅇ)) =>
+        Vector(B)
+      case (HangeulLetter.ㅂ, Some(HangeulLetter.ㄴ) | Some(HangeulLetter.ㄹ) | Some(HangeulLetter.ㅁ)) =>
+        Vector(M)
+      case (HangeulLetter.ㅅ, Some(HangeulLetter.ㅇ)) =>
+        Vector(S)
+      case (HangeulLetter.ㅅ, Some(HangeulLetter.ㄴ) | Some(HangeulLetter.ㄹ) | Some(HangeulLetter.ㅁ)) =>
+        Vector(N)
       case _ =>
         transliterateFinal(`final`)
+    }
+
+  private def transliterateInitialInContext(
+    maybePrecedingFinal: Option[HangeulLetter.Consonant],
+    initial: HangeulLetter.Consonant
+  ): Vector[RomanLetter] =
+    (maybePrecedingFinal, initial) match {
+      case (Some(HangeulLetter.ㄱ) | Some(HangeulLetter.ㄷ) | Some(HangeulLetter.ㅂ), HangeulLetter.ㄹ) =>
+        Vector(N)
+      case (Some(HangeulLetter.ㄹ), HangeulLetter.ㄴ) =>
+        Vector(L) // This could also sometimes be NN instead of LL
+      case (Some(HangeulLetter.ㄹ), HangeulLetter.ㄹ) =>
+        Vector(L)
+      case (Some(HangeulLetter.ㅅ) | Some(HangeulLetter.ㅇ), HangeulLetter.ㄹ) =>
+        Vector(N)
+      case (Some(HangeulLetter.ㅂ), HangeulLetter.ㅁ) =>
+        Vector(M)
+      case _ =>
+        transliterateInitial(initial)
     }
 
 }
--- src/test/scala/com/github/sophiecollard/transliterator/transliteration/HangeulRomanizerSpec.scala
+++ src/test/scala/com/github/sophiecollard/transliterator/transliteration/HangeulRomanizerSpec.scala
@@ -188,6 +188,54 @@ class HangeulRomanizerSpec extends Specification {
 
     "romanize words with irregular consonant clusters, such as" in {
 
+      "'성산읍' to 'seongsaneup'" in {
+        val input = HangeulText(
+          Vector(
+            HangeulWord(
+              Vector(
+                HangeulSyllabicBlock.IMF(
+                  HangeulLetter.ㅅ,
+                  HangeulLetter.ㅓ,
+                  HangeulLetter.ㅇ
+                ),
+                HangeulSyllabicBlock.IMF(
+                  HangeulLetter.ㅅ,
+                  HangeulLetter.ㅏ,
+                  HangeulLetter.ㄴ
+                ),
+                HangeulSyllabicBlock.IMF(
+                  HangeulLetter.ㅇ,
+                  HangeulLetter.ㅡ,
+                  HangeulLetter.ㅂ
+                )
+              )
+            )
+          )
+        )
+
+        val expectedOutput = RomanizedText(
+          Vector(
+            RomanizedWord(
+              Vector(
+                RomanLetter.S,
+                RomanLetter.E,
+                RomanLetter.O,
+                RomanLetter.N,
+                RomanLetter.G,
+                RomanLetter.S,
+                RomanLetter.A,
+                RomanLetter.N,
+                RomanLetter.E,
+                RomanLetter.U,
+                RomanLetter.P
+              )
+            )
+          )
+        )
+
+        HangeulRomanizer.transliterate(input) must beRight(expectedOutput)
+      }
+
       "'설악' to 'seorak'" in {
         val input = HangeulText(
           Vector(
@@ -218,6 +266,62 @@ class HangeulRomanizerSpec extends Specification {
                 RomanLetter.R,
                 RomanLetter.A,
                 RomanLetter.K
+              )
+            )
+          )
+        )
+
+        HangeulRomanizer.transliterate(input) must beRight(expectedOutput)
+      }
+
+      "'감사합니다' to 'gamsahamnida'" in {
+        val input = HangeulText(
+          Vector(
+            HangeulWord(
+              Vector(
+                HangeulSyllabicBlock.IMF(
+                  HangeulLetter.ㄱ,
+                  HangeulLetter.ㅏ,
+                  HangeulLetter.ㅁ
+                ),
+                HangeulSyllabicBlock.IM(
+                  HangeulLetter.ㅅ,
+                  HangeulLetter.ㅏ
+                ),
+                HangeulSyllabicBlock.IMF(
+                  HangeulLetter.ㅎ,
+                  HangeulLetter.ㅏ,
+                  HangeulLetter.ㅂ
+                ),
+                HangeulSyllabicBlock.IM(
+                  HangeulLetter.ㄴ,
+                  HangeulLetter.ㅣ
+                ),
+                HangeulSyllabicBlock.IM(
+                  HangeulLetter.ㄷ,
+                  HangeulLetter.ㅏ
+                )
+              )
+            )
+          )
+        )
+
+        val expectedOutput = RomanizedText(
+          Vector(
+            RomanizedWord(
+              Vector(
+                RomanLetter.G,
+                RomanLetter.A,
+                RomanLetter.M,
+                RomanLetter.S,
+                RomanLetter.A,
+                RomanLetter.H,
+                RomanLetter.A,
+                RomanLetter.M,
+                RomanLetter.N,
+                RomanLetter.I,
+                RomanLetter.D,
+                RomanLetter.A
               )
             )
           )
@@ -296,6 +400,49 @@ class HangeulRomanizerSpec extends Specification {
                 RomanLetter.U,
                 RomanLetter.N,
                 RomanLetter.G
+              )
+            )
+          )
+        )
+
+        HangeulRomanizer.transliterate(input) must beRight(expectedOutput)
+      }
+
+      "'종로구' to 'jongnogu'" in {
+        val input = HangeulText(
+          Vector(
+            HangeulWord(
+              Vector(
+                HangeulSyllabicBlock.IMF(
+                  HangeulLetter.ㅈ,
+                  HangeulLetter.ㅗ,
+                  HangeulLetter.ㅇ
+                ),
+                HangeulSyllabicBlock.IM(
+                  HangeulLetter.ㄹ,
+                  HangeulLetter.ㅗ
+                ),
+                HangeulSyllabicBlock.IM(
+                  HangeulLetter.ㄱ,
+                  HangeulLetter.ㅜ
+                )
+              )
+            )
+          )
+        )
+
+        val expectedOutput = RomanizedText(
+          Vector(
+            RomanizedWord(
+              Vector(
+                RomanLetter.J,
+                RomanLetter.O,
+                RomanLetter.N,
+                RomanLetter.G,
+                RomanLetter.N,
+                RomanLetter.O,
+                RomanLetter.G,
+                RomanLetter.U
               )
             )
           )

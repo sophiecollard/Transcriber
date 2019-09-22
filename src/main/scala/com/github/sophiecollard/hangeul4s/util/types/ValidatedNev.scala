55227bfd324b649dd9d3cdb5e56f63262da6e192
Sophie Collard <sophie.collard@hotmail.fr>
2019-09-22 14:27:37 +0100

    Implement ValidatedNev#flatMap

--- src/main/scala/com/github/sophiecollard/hangeul4s/util/types/ValidatedNev.scala
+++ src/main/scala/com/github/sophiecollard/hangeul4s/util/types/ValidatedNev.scala
@@ -7,10 +7,28 @@ sealed abstract class ValidatedNev[+E, +A] {
   import ValidatedNev._
 
   def map[B](f: A => B): ValidatedNev[E, B] =
-    functor.map(this, f)
+    this match {
+      case Valid(a)    => Valid(f(a))
+      case Invalid(es) => Invalid(es)
+    }
+
+  def flatMap[EE >: E, B](f: A => ValidatedNev[EE, B]): ValidatedNev[EE, B] =
+    this match {
+      case Valid(a)    => f(a)
+      case Invalid(es) => Invalid(es)
+    }
 
   def product[EE >: E, B](fb: ValidatedNev[EE, B]): ValidatedNev[EE, (A, B)] =
-    semigroupal.product(this, fb)
+    (this, fb) match {
+      case (Valid(a), Valid(b)) =>
+        Valid((a, b))
+      case (Invalid(es1), Invalid(es2)) =>
+        Invalid(es1 concat es2)
+      case (Invalid(es), Valid(_)) =>
+        Invalid(es)
+      case (Valid(_), Invalid(es)) =>
+        Invalid(es)
+    }
 
   def toEither: Either[NonEmptyVector[E], A] = this match {
     case Invalid(es) => Left(es)
@@ -39,25 +57,13 @@ object ValidatedNev {
   implicit def functor[E]: Functor[ValidatedNev[E, ?]] =
     new Functor[ValidatedNev[E, ?]] {
       override def map[A, B](fa: ValidatedNev[E, A], f: A => B): ValidatedNev[E, B] =
-        fa match {
-          case Valid(a)    => Valid(f(a))
-          case Invalid(es) => Invalid(es)
-        }
+        fa.map(f)
     }
 
   implicit def semigroupal[E]: Semigroupal[ValidatedNev[E, ?]] =
     new Semigroupal[ValidatedNev[E, ?]] {
       override def product[A, B](fa: ValidatedNev[E, A], fb: ValidatedNev[E, B]): ValidatedNev[E, (A, B)] =
-        (fa, fb) match {
-          case (Valid(a), Valid(b)) =>
-            Valid((a, b))
-          case (Invalid(es1), Invalid(es2)) =>
-            Invalid(es1 concat es2)
-          case (Invalid(es), Valid(_)) =>
-            Invalid(es)
-          case (Valid(_), Invalid(es)) =>
-            Invalid(es)
-        }
+        fa.product(fb)
     }
 
   implicit def applicative[E]: Applicative[ValidatedNev[E, ?]] =

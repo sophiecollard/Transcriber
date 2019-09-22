3aad8f6940f77940ebc89395f720f49c08702f2b
Sophie Collard <sophie.collard@hotmail.fr>
2019-09-22 14:04:50 +0100

    Introduce ParallelParser

--- src/main/scala/com/github/sophiecollard/hangeul4s/parsing/Parser.scala
+++ src/main/scala/com/github/sophiecollard/hangeul4s/parsing/Parser.scala
@@ -1,7 +1,5 @@
 package com.github.sophiecollard.hangeul4s.parsing
 
-import com.github.sophiecollard.hangeul4s.error.ParsingError
-
 trait Parser[A] {
 
   def parse(input: String): ParsingResult[A]
@@ -10,9 +8,9 @@ trait Parser[A] {
 
 object Parser {
 
-  def instance[A](f: String => Either[ParsingError, A]): Parser[A] =
+  def instance[A](f: String => ParsingResult[A]): Parser[A] =
     new Parser[A] {
-      override def parse(input: String): Either[ParsingError, A] =
+      override def parse(input: String): ParsingResult[A] =
         f(input)
     }
 
--- src/main/scala/com/github/sophiecollard/hangeul4s/parsing/package.scala
+++ src/main/scala/com/github/sophiecollard/hangeul4s/parsing/package.scala
@@ -1,16 +1,23 @@
 package com.github.sophiecollard.hangeul4s
 
 import com.github.sophiecollard.hangeul4s.error.ParsingError
+import com.github.sophiecollard.hangeul4s.util.types.ValidatedNev
+import com.github.sophiecollard.hangeul4s.util.types.ValidatedNev.Valid
 
 package object parsing {
 
   type ParsingResult[A] = Either[ParsingError, A]
 
   object ParsingResult {
-
     def success[A](value: A): ParsingResult[A] =
       Right(value)
+  }
 
+  type ParallelParsingResult[A] = ValidatedNev[ParsingError, A]
+
+  object ParallelParsingResult {
+    def success[A](value: A): ParallelParsingResult[A] =
+      Valid(value)
   }
 
 }

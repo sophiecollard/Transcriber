bf5fafe664cd0d6c52c66470c0ca0ef3bdc5d9f5
Sophie Collard <sophie.collard@hotmail.fr>
2019-09-22 15:58:42 +0100

    Refactor HangeulTextElementSpec

--- src/test/scala/com/github/sophiecollard/hangeul4s/model/hangeul/HangeulTextElementSpec.scala
+++ src/test/scala/com/github/sophiecollard/hangeul4s/model/hangeul/HangeulTextElementSpec.scala
@@ -1,7 +1,8 @@
 package com.github.sophiecollard.hangeul4s.model.hangeul
 
+import com.github.sophiecollard.hangeul4s.model.hangeul.HangeulJamo._
+import com.github.sophiecollard.hangeul4s.model.hangeul.HangeulSyllabicBlock._
 import com.github.sophiecollard.hangeul4s.error.ParsingError
-import com.github.sophiecollard.hangeul4s.util.types.NonEmptyVector
 import org.specs2.mutable.Specification
 
 class HangeulTextElementSpec extends Specification {
@@ -11,31 +12,12 @@ class HangeulTextElementSpec extends Specification {
     "parse words made of characters from the Hangeul Syllables Unicode block" in {
       val input = "안녕하세요"
 
-      val expectedOutput: HangeulTextElement = HangeulTextElement.Word(
-        NonEmptyVector.of(
-          HangeulSyllabicBlock.ThreeLetter(
-            HangeulJamo.Initial.ㅇ,
-            HangeulJamo.Medial.ㅏ,
-            HangeulJamo.Final.ㄴ
-          ),
-          HangeulSyllabicBlock.ThreeLetter(
-            HangeulJamo.Initial.ㄴ,
-            HangeulJamo.Medial.ㅕ,
-            HangeulJamo.Final.ㅇ
-          ),
-          HangeulSyllabicBlock.TwoLetter(
-            HangeulJamo.Initial.ㅎ,
-            HangeulJamo.Medial.ㅏ
-          ),
-          HangeulSyllabicBlock.TwoLetter(
-            HangeulJamo.Initial.ㅅ,
-            HangeulJamo.Medial.ㅔ
-          ),
-          HangeulSyllabicBlock.TwoLetter(
-            HangeulJamo.Initial.ㅇ,
-            HangeulJamo.Medial.ㅛ
-          )
-        )
+      val expectedOutput: HangeulTextElement = HangeulTextElement.Word.fromSyllabicBlocks(
+        ThreeLetter(Initial.ㅇ, Medial.ㅏ, Final.ㄴ),
+        ThreeLetter(Initial.ㄴ, Medial.ㅕ, Final.ㅇ),
+        TwoLetter(Initial.ㅎ, Medial.ㅏ),
+        TwoLetter(Initial.ㅅ, Medial.ㅔ),
+        TwoLetter(Initial.ㅇ, Medial.ㅛ)
       )
 
       HangeulTextElement.parser.parse(input) must beRight(expectedOutput)

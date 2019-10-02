62936ff82bf714c01d36ea30acddfab16eaeaf59
Sophie Collard <sophie.collard@hotmail.fr>
2019-10-02 21:24:13 +0100

    Drop SequentialValidationResult type alias

--- src/main/scala/com/github/sophiecollard/hangeul4s/validation/package.scala
+++ src/main/scala/com/github/sophiecollard/hangeul4s/validation/package.scala
@@ -1,12 +1,10 @@
 package com.github.sophiecollard.hangeul4s
 
 import com.github.sophiecollard.hangeul4s.error.ValidationError
-import com.github.sophiecollard.hangeul4s.util.types.{NonEmptyVector, ValidatedNev}
+import com.github.sophiecollard.hangeul4s.util.types.ValidatedNev
 
 package object validation {
 
   type ValidationResult[A] = ValidatedNev[ValidationError, A]
-
-  type SequentialValidationResult[A] = Either[NonEmptyVector[ValidationError], A]
 
 }

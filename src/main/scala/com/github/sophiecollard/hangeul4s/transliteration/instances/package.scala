be78aa65402303cf25a7edc0e4cd80d81fb816e1
Sophie Collard <sophie.collard@ovoenergy.com>
2019-10-04 21:36:33 +0100

    Afford deriving Transliterator[F[I], F[O]] instances

---
+++ src/main/scala/com/github/sophiecollard/hangeul4s/transliteration/instances/package.scala
@@ -0,0 +1,17 @@
+package com.github.sophiecollard.hangeul4s.transliteration
+
+import cats.Traverse
+import cats.instances.either._
+import cats.syntax.functor._
+import cats.syntax.traverse._
+
+package object instances {
+
+  implicit def transliteratorF[F[_]: Traverse, I, O](
+    implicit transliterator: Transliterator[I, O]
+  ): Transliterator[F[I], F[O]] =
+    Transliterator.instance { input =>
+      input.map(transliterator.transliterate).sequence
+    }
+
+}

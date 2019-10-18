c38692664cd2599be17ebb78efb3bca666531b48
Sophie Collard <sophie.collard@ovoenergy.com>
2019-10-18 10:54:58 +0100

    Split HangeulSyllable charCodec into charDecoder and charEncoder

--- core/src/main/scala/hangeul4s/model/hangeul/HangeulSyllable.scala
+++ core/src/main/scala/hangeul4s/model/hangeul/HangeulSyllable.scala
@@ -3,7 +3,7 @@ package hangeul4s.model.hangeul
 import java.text.Normalizer
 
 import cats.syntax.either._ // required for toOption method in Scala 2.11
-import hangeul4s.encoding.{Codec, Decoder, Encoder}
+import hangeul4s.encoding.{Decoder, Encoder}
 import hangeul4s.error.DecodingFailure
 import hangeul4s.syntax.string.StringOps
 
@@ -16,12 +16,12 @@ sealed trait HangeulSyllable {
 
   def medial: HangeulJamo.Medial
 
-  def maybeFinal: Option[HangeulJamo.Final] = this match {
+  final def maybeFinal: Option[HangeulJamo.Final] = this match {
     case HangeulSyllable.TwoJamo(_, _)      => None
     case HangeulSyllable.ThreeJamo(_, _, f) => Some(f)
   }
 
-  def charSequence: CharSequence = this match {
+  final def toJamoString: String = this match {
     case HangeulSyllable.TwoJamo(i, m)      => s"${i.char}${m.char}"
     case HangeulSyllable.ThreeJamo(i, m, f) => s"${i.char}${m.char}${f.char}"
   }
@@ -59,37 +59,27 @@ object HangeulSyllable {
   ): HangeulSyllable =
     ThreeJamo(initial, medial, _final)
 
-  /**
-    * Instance for decoding/encoding [[HangeulSyllable]] from/to Char
-    *
-    * The decimal code point of precomposed Hangeul syllables in the Hangul Syllables Unicode block can be derived from
-    * the indices of their constituent [[HangeulJamo]] characters using the following formula:
-    *
-    *   ((initial) × 588 + (medial) × 28 + (final)) + 44032
-    *
-    * See https://en.wikipedia.org/wiki/Korean_language_and_computers#Hangul_in_Unicode
-    */
-  implicit val charCodec: Codec[Char, HangeulSyllable] =
-    new Codec[Char, HangeulSyllable] {
-      override def decode(encoded: Char): Either[DecodingFailure, HangeulSyllable] = {
-        val decomposition = Normalizer.normalize(encoded.toString, Normalizer.Form.NFD)
+  implicit val charDecoder: Decoder[Char, HangeulSyllable] =
+    Decoder.instance { char =>
+      val decomposition = Normalizer.normalize(char.toString, Normalizer.Form.NFD)
 
-        (
-          decomposition.safeCharAt(0).flatMap(Decoder[Char, HangeulJamo.Initial].decode(_).toOption),
-          decomposition.safeCharAt(1).flatMap(Decoder[Char, HangeulJamo.Medial].decode(_).toOption),
-          decomposition.safeCharAt(2).flatMap(Decoder[Char, HangeulJamo.Final].decode(_).toOption)
-        ) match {
-          case (Some(initial), Some(medial), None) =>
-            Right(HangeulSyllable.TwoJamo(initial, medial))
-          case (Some(initial), Some(medial), Some(_final)) =>
-            Right(HangeulSyllable.ThreeJamo(initial, medial, _final))
-          case _ =>
-            Left(DecodingFailure.FailedToDecodeHangeulSyllable(encoded))
-        }
+      (
+        decomposition.safeCharAt(0).flatMap(Decoder[Char, HangeulJamo.Initial].decode(_).toOption),
+        decomposition.safeCharAt(1).flatMap(Decoder[Char, HangeulJamo.Medial].decode(_).toOption),
+        decomposition.safeCharAt(2).flatMap(Decoder[Char, HangeulJamo.Final].decode(_).toOption)
+      ) match {
+        case (Some(initial), Some(medial), None) =>
+          Right(HangeulSyllable.TwoJamo(initial, medial))
+        case (Some(initial), Some(medial), Some(_final)) =>
+          Right(HangeulSyllable.ThreeJamo(initial, medial, _final))
+        case _ =>
+          Left(DecodingFailure.FailedToDecodeHangeulSyllable(char))
       }
+    }
 
-      override def encode(decoded: HangeulSyllable): Char =
-        Normalizer.normalize(decoded.charSequence, Normalizer.Form.NFC).charAt(0)
+  implicit val charEncoder: Encoder[HangeulSyllable, Char] =
+    Encoder.instance { syllable =>
+      Normalizer.normalize(syllable.toJamoString, Normalizer.Form.NFC).charAt(0)
     }
 
 }

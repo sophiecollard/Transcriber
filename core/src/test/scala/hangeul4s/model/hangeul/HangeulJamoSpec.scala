55ae8cee99365249faf190d39662cb5843fd0410
Sophie Collard <sophie.collard@ovoenergy.com>
2019-10-18 22:53:59 +0100

    Improve test coverage of HangeulJamo.charDecoder

--- core/src/test/scala/hangeul4s/model/hangeul/HangeulJamoSpec.scala
+++ core/src/test/scala/hangeul4s/model/hangeul/HangeulJamoSpec.scala
@@ -1,8 +1,12 @@
 package hangeul4s.model.hangeul
 
 import cats.Show
+import cats.instances.either._
+import cats.instances.vector._
+import cats.syntax.traverse._
 import hangeul4s.encoding.syntax._
 import hangeul4s.error.DecodingFailure
+import hangeul4s.model.hangeul.HangeulJamo._
 import org.specs2.mutable.Specification
 
 class HangeulJamoSpec extends Specification {
@@ -10,15 +14,15 @@ class HangeulJamoSpec extends Specification {
   "HangeulJamo#toString" should {
 
     "return a String representation of a HangeulJamo.Initial instance" in {
-      HangeulJamo.Initial.ㄲ.toString ==== "hangeul4s.model.hangeul.HangeulJamo.Initial(ᄁ)"
+      Initial.ㄲ.toString ==== "hangeul4s.model.hangeul.HangeulJamo.Initial(ᄁ)"
     }
 
-    "return a String representation of a HangeulJamo.Medial instance" in {
-      HangeulJamo.Medial.ㅐ.toString === "hangeul4s.model.hangeul.HangeulJamo.Medial(ᅢ)"
+    "return a String representation of a Medial instance" in {
+      Medial.ㅐ.toString === "hangeul4s.model.hangeul.HangeulJamo.Medial(ᅢ)"
     }
 
-    "return a String representation of a HangeulJamo.Final instance" in {
-      HangeulJamo.Final.ㄲ.toString ==== "hangeul4s.model.hangeul.HangeulJamo.Final(ᆩ)"
+    "return a String representation of a Final instance" in {
+      Final.ㄲ.toString ==== "hangeul4s.model.hangeul.HangeulJamo.Final(ᆩ)"
     }
 
   }
@@ -26,34 +30,66 @@ class HangeulJamoSpec extends Specification {
   "HangeulJamo#show" should {
 
     "return the character representing an initial consonant" in {
-      Show[HangeulJamo.Initial].show(HangeulJamo.Initial.ㄲ) ==== "ᄁ"
-      Show[HangeulJamo].show(HangeulJamo.Initial.ㄲ) ==== "ᄁ"
+      Show[Initial].show(Initial.ㄲ) ==== "ᄁ"
+      Show[HangeulJamo].show(Initial.ㄲ) ==== "ᄁ"
     }
 
     "return the character representing a medial" in {
-      Show[HangeulJamo.Medial].show(HangeulJamo.Medial.ㅐ) ==== "ᅢ"
-      Show[HangeulJamo].show(HangeulJamo.Medial.ㅐ) ==== "ᅢ"
+      Show[Medial].show(Medial.ㅐ) ==== "ᅢ"
+      Show[HangeulJamo].show(Medial.ㅐ) ==== "ᅢ"
     }
 
     "return the character representing a final consonant" in {
-      Show[HangeulJamo.Final].show(HangeulJamo.Final.ㄲ) ==== "ᆩ"
-      Show[HangeulJamo].show(HangeulJamo.Final.ㄲ) ==== "ᆩ"
+      Show[Final].show(Final.ㄲ) ==== "ᆩ"
+      Show[HangeulJamo].show(Final.ㄲ) ==== "ᆩ"
     }
 
   }
 
   "HangeulJamo#charDecoder" should {
 
-    "decode an initial consonant" in {
-      'ᄀ'.decodeTo[HangeulJamo] should beRight[HangeulJamo](HangeulJamo.Initial.ㄱ)
+    "decode initial consonants" in {
+      val encoded = Vector(
+        'ᄀ', 'ᄁ', 'ᄂ', 'ᄃ', 'ᄄ', 'ᄅ', 'ᄆ', 'ᄇ', 'ᄈ', 'ᄉ', 'ᄊ', 'ᄋ', 'ᄌ', 'ᄍ', 'ᄎ', 'ᄏ', 'ᄐ', 'ᄑ', 'ᄒ'
+      )
+
+      val decoded = Vector[HangeulJamo](
+        Initial.ㄱ, Initial.ㄲ, Initial.ㄴ, Initial.ㄷ, Initial.ㄸ, Initial.ㄹ, Initial.ㅁ, Initial.ㅂ, Initial.ㅃ,
+        Initial.ㅅ, Initial.ㅆ, Initial.ㅇ, Initial.ㅈ, Initial.ㅉ, Initial.ㅊ, Initial.ㅋ, Initial.ㅌ, Initial.ㅍ,
+        Initial.ㅎ
+      )
+
+      encoded.map(_.decodeTo[HangeulJamo]).sequence should beRight(decoded)
     }
 
-    "decode a medial" in {
-      'ᅡ'.decodeTo[HangeulJamo] should beRight[HangeulJamo](HangeulJamo.Medial.ㅏ)
+    "decode medials" in {
+      val encoded = Vector(
+        'ᅡ', 'ᅢ', 'ᅣ', 'ᅤ', 'ᅥ', 'ᅦ', 'ᅧ', 'ᅨ', 'ᅩ', 'ᅪ', 'ᅫ', 'ᅬ', 'ᅭ', 'ᅮ', 'ᅯ', 'ᅰ', 'ᅱ', 'ᅲ', 'ᅳ', 'ᅴ',
+        'ᅵ'
+      )
+
+      val decoded = Vector[HangeulJamo](
+        Medial.ㅏ, Medial.ㅐ, Medial.ㅑ, Medial.ㅒ, Medial.ㅓ, Medial.ㅔ, Medial.ㅕ, Medial.ㅖ, Medial.ㅗ, Medial.ㅘ,
+        Medial.ㅙ, Medial.ㅚ, Medial.ㅛ, Medial.ㅜ, Medial.ㅝ, Medial.ㅞ, Medial.ㅟ, Medial.ㅠ, Medial.ㅡ, Medial.ㅢ,
+        Medial.ㅣ
+      )
+
+      encoded.map(_.decodeTo[HangeulJamo]).sequence should beRight(decoded)
     }
 
-    "decode a final consonant" in {
-      'ᆨ'.decodeTo[HangeulJamo] should beRight[HangeulJamo](HangeulJamo.Final.ㄱ)
+    "decode final consonants" in {
+      val encoded = Vector(
+        'ᆨ', 'ᆩ', 'ᆪ', 'ᆫ', 'ᆬ', 'ᆭ', 'ᆮ', 'ᆯ', 'ᆰ', 'ᆱ', 'ᆲ', 'ᆳ', 'ᆴ', 'ᆵ', 'ᆶ', 'ᆷ', 'ᆸ', 'ᆹ', 'ᆺ', 'ᆻ',
+        'ᆼ', 'ᆽ', 'ᆾ', 'ᆿ', 'ᇀ', 'ᇁ', 'ᇂ'
+      )
+
+      val decoded = Vector[HangeulJamo](
+        Final.ㄱ, Final.ㄲ, Final.ㄳ, Final.ㄴ, Final.ㄵ, Final.ㄶ, Final.ㄷ, Final.ㄹ, Final.ㄺ, Final.ㄻ, Final.ㄼ,
+        Final.ㄽ, Final.ㄾ, Final.ㄿ, Final.ㅀ, Final.ㅁ, Final.ㅂ, Final.ㅄ, Final.ㅅ, Final.ㅆ, Final.ㅇ, Final.ㅈ,
+        Final.ㅊ, Final.ㅋ, Final.ㅌ, Final.ㅍ, Final.ㅎ
+      )
+
+      encoded.map(_.decodeTo[HangeulJamo]).sequence should beRight(decoded)
     }
 
     "fail to decode archaic jamo" in {
@@ -65,15 +101,15 @@ class HangeulJamoSpec extends Specification {
   "HangeulJamo#charEncoder" should {
 
     "encode an initial consonant" in {
-      (HangeulJamo.Initial.ㅎ: HangeulJamo).encodeTo[Char] ==== 'ᄒ'
+      (Initial.ㅎ: HangeulJamo).encodeTo[Char] ==== 'ᄒ'
     }
 
     "encode a medial" in {
-      (HangeulJamo.Medial.ㅣ: HangeulJamo).encodeTo[Char] ==== 'ᅵ'
+      (Medial.ㅣ: HangeulJamo).encodeTo[Char] ==== 'ᅵ'
     }
 
     "encode a final consonant" in {
-      (HangeulJamo.Final.ㅎ: HangeulJamo).encodeTo[Char] ==== 'ᇂ'
+      (Final.ㅎ: HangeulJamo).encodeTo[Char] ==== 'ᇂ'
     }
 
   }

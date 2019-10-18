278c1e29239f0f5e20b84ba6fc9fe824a7d76c5f
Sophie Collard <sophie.collard@ovoenergy.com>
2019-10-18 10:06:50 +0100

    Rename methods from encoding syntax

--- core/src/main/scala/hangeul4s/encoding/syntax/Syntax.scala
+++ core/src/main/scala/hangeul4s/encoding/syntax/Syntax.scala
@@ -6,12 +6,12 @@ import hangeul4s.error.DecodingFailure
 trait Syntax {
 
   implicit class DecoderOps[A](value: A) {
-    def decodeAs[B](implicit ev: Decoder[A, B]): Either[DecodingFailure, B] =
+    def decodeTo[B](implicit ev: Decoder[A, B]): Either[DecodingFailure, B] =
       ev.decode(value)
   }
 
   implicit class EncoderOps[B](value: B) {
-    def encodeAs[A](implicit ev: Encoder[B, A]): A =
+    def encodeTo[A](implicit ev: Encoder[B, A]): A =
       ev.encode(value)
   }
 

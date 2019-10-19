ec50e53e58f63c39a37479fc33636ac413dce244
Sophie Collard <sophie.collard@ovoenergy.com>
2019-10-19 17:06:36 +0100

    Test encoding syntax

--- core/src/main/scala/hangeul4s/encoding/syntax/Syntax.scala
+++ core/src/main/scala/hangeul4s/encoding/syntax/Syntax.scala
@@ -5,12 +5,12 @@ import hangeul4s.error.DecodingFailure
 
 trait Syntax {
 
-  implicit class DecoderOps[A](value: A) {
+  implicit class DecodingOps[A](value: A) {
     def decodeTo[B](implicit decoder: Decoder[A, B]): Either[DecodingFailure, B] =
       decoder.decode(value)
   }
 
-  implicit class EncoderOps[B](value: B) {
+  implicit class EncodingOps[B](value: B) {
     def encodeTo[A](implicit encoder: Encoder[B, A]): A =
       encoder.encode(value)
   }
